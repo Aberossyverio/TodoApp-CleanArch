762129afb7e7ff73564d5287d4a0504841a609a4
Verio Aberossy R. <verio.renedominick@gmail.com>
2024-09-17 18:49:02 +0700

    feat infrastructure : Register ITodoItemService & TodoItemService to InfrastructureServiceExtensions

--- TodoApp/src/TodoApp.Infrastructure/InfrastructureServiceExtensions.cs
+++ TodoApp/src/TodoApp.Infrastructure/InfrastructureServiceExtensions.cs
@@ -10,6 +10,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using TodoApp.UseCases.TodoItem.Interfaces;
+using TodoApp.Infrastructure.TodoItem;
 
 namespace TodoApp.Infrastructure;
 public static class InfrastructureServiceExtensions
@@ -21,13 +23,16 @@ public static class InfrastructureServiceExtensions
   {
     string? connectionString = config.GetConnectionString("SqliteConnection");
     Guard.Against.Null(connectionString);
-    services.AddDbContext<AppDbContext>(options =>
-     options.UseSqlite(connectionString));
+    services.AddDbContext<AppDbContext>(options => {
+        options.UseSqlite(connectionString);
+    });
 
     services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
     services.AddScoped(typeof(IReadRepository<>), typeof(EfRepository<>));
     services.AddScoped<IListContributorsQueryService, ListContributorsQueryService>();
     services.AddScoped<IDeleteContributorService, DeleteContributorService>();
+    services.AddScoped<ITodoItemService, TodoItemService>();
+
 
     services.Configure<MailserverConfiguration>(config.GetSection("Mailserver"));
 
