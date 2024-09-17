140358c58c0ec77e25202499896da64a2df50550
Verio Aberossy R. <verio.renedominick@gmail.com>
2024-09-17 18:07:50 +0700

    fix core : Fix TodoItemCreatedEvent to using primary constructor

--- TodoApp/src/TodoApp.Core/TodoItem/Events/TodoItemCreatedEvent.cs
+++ TodoApp/src/TodoApp.Core/TodoItem/Events/TodoItemCreatedEvent.cs
@@ -1,18 +1,9 @@
 namespace TodoApp.Core.TodoItem.Events;
 
-  public class TodoItemCreatedEvent
-  {
-      public int Id { get; }
-      public string Title { get; }
-      public string Description { get; }
-      public DateTime CreatedDate { get; }
-      
-      // Constructor
-      public TodoItemCreatedEvent(int id, string title, string description, DateTime createdDate)
-      {
-          Id = id;
-          Title = title;
-          Description = description;
-          CreatedDate = createdDate;
-      }
-  }
+  public class TodoItemCreatedEvent(int id, string title, string description, DateTime createdDate)
+{
+  public int Id { get; } = id;
+  public string Title { get; } = title;
+  public string Description { get; } = description;
+  public DateTime CreatedDate { get; } = createdDate;
+}
